e347ab566eaf93ea896fa504f3c7ea39f7e40143
mustafatugrul <mustafatugrul034@gmail.com>
2025-02-08 17:24:57 +0300

    Bug fixed

---
+++ src/projects/MovieProjects.DataAccess/Configurations/MovieArtistConfiguration.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MovieProject.Model.Entities;
+
+namespace MovieProject.DataAccess.Configurations;
+
+public sealed class MovieArtistConfiguration : IEntityTypeConfiguration<MovieArtist>
+{
+    public void Configure(EntityTypeBuilder<MovieArtist> builder)
+    {
+        builder.ToTable("MovieArtists").HasKey(x => x.Id);
+        builder.Property(x => x.Id).HasColumnName("MovieArtistsId").IsRequired();
+        builder.Property(x => x.CreatedTime).HasColumnName("Created").IsRequired();
+
+        builder.HasOne(x => x.Artist).WithMany(x => x.MovieArtists).HasForeignKey(x => x.ArtistId);
+
+        builder.HasOne(x => x.Movie).WithMany(x => x.MovieArtists).HasForeignKey(x => x.MovieId);
+    }
+}
